1585284d81dab331e98ead23c8323bb235e5215c
Cora Schallock <coraschallock@gmail.com>
2023-10-25 16:43:18 -0700

    small changes to path variables in run_gofher

--- gofher/run_gofher.py
+++ gofher/run_gofher.py
@@ -5,18 +5,25 @@ from spin_parity import read_spin_parity_galaxies_label_from_csv
 from run_gofher_on_a_galaxy import run_sdss, run_panstarrs
 from sdss import create_sdss_csv, SDSS_BANDS_IN_ORDER
 from panstarrs import create_panstarrs_csv, PANSTARRS_BANDS_IN_ORDER
+from file_helper import check_if_folder_exists_and_create
 
-
-path_to_input = "C:\\Users\\school\\Desktop\\github\\spin-parity-catalog\\original\\galaxies\\" #PANSTARRS
-#path_to_input = "C:\\Users\\school\\Desktop\\cross_id\\sdss_mosaic_construction" #SDSS
-folder_name = "table5"
-
-path_to_output = "C:\\Users\\school\\Desktop\\gofher_output_refactor"
-#output_folder_name = "gofher_SDSS_mosaic_bounds_test"
-output_folder_name = 'gofher_panstarrs_bounds_test'
+use_panstarrs = True
 
 visualize_gals=False
 make_csv=True
+
+if use_panstarrs:
+    path_to_input = "C:\\Users\\school\\Desktop\\github\\spin-parity-catalog\\original\\galaxies\\" #PANSTARRS
+else:
+    path_to_input = "C:\\Users\\school\\Desktop\\cross_id\\sdss_mosaic_construction" #SDSS
+
+if use_panstarrs:
+    output_folder_name = "gofher_panstarrs_bounds_test" #PANSTARRS
+else:
+    output_folder_name = "gofher_SDSS_mosaic_bounds_test" #SDSS
+
+folder_name = "table5"
+path_to_output = "C:\\Users\\school\\Desktop\\gofher_output_refactor"
 
 def get_fits_path(name,band):
     """the file path of where existing fits files can be found"""
@@ -41,11 +48,20 @@ def get_dark_side_csv_path():
     csv_path = "C:\\Users\\school\\Desktop\\github\\spin-parity-catalog\\table_info\\csv_format_of_table\\"
     return os.path.join(csv_path,"table_{}.csv".format(folder_name.strip()[-1]))
 
+def setup_output_directories():
+    output_path = os.path.join(path_to_output, output_folder_name)
+    check_if_folder_exists_and_create(output_path)
+
+    output_folder_path = os.path.join(path_to_output, output_folder_name, folder_name)
+    check_if_folder_exists_and_create(output_folder_path)
+
 
 if __name__ == "__main__":
     dark_side_labels = read_spin_parity_galaxies_label_from_csv(get_dark_side_csv_path())
     if folder_name == "table2" and "IC 2101" in dark_side_labels:
         dark_side_labels["IC2101"] = dark_side_labels["IC 2101"]
+
+    setup_output_directories()
 
     the_gals = []
     i = 1
@@ -58,13 +74,18 @@ if __name__ == "__main__":
                 save_vis_path=get_save_vis_path(name)
 
             paper_dark_side_label = dark_side_labels[name]
-            the_gal = run_panstarrs(name, get_fits_path, save_vis_path=save_vis_path,dark_side_label=paper_dark_side_label,color_image_path=get_color_image_path(name))#PANSTARRS
-            #the_gal = run_sdss(name, get_fits_path, save_vis_path=save_vis_path, dark_side_label='') #SDSS
+            if use_panstarrs:
+                the_gal = run_panstarrs(name, get_fits_path, save_vis_path=save_vis_path,dark_side_label=paper_dark_side_label,color_image_path=get_color_image_path(name)) #PANSTARRS
+            else:
+                the_gal = run_sdss(name, get_fits_path, save_vis_path=save_vis_path, dark_side_label='') #SDSS
             the_gals.append(the_gal)
         except:
             print("Error when running on",name)
 
     if make_csv:
-        the_band_pairs = list(itertools.combinations(PANSTARRS_BANDS_IN_ORDER, 2)) #PANSTARRS
-        #the_band_pairs = list(itertools.combinations(SDSS_BANDS_IN_ORDER, 2)) #SDSS
-        create_panstarrs_csv(the_gals,the_band_pairs,get_csv_out_path())
+        if use_panstarrs:
+            the_band_pairs = list(itertools.combinations(PANSTARRS_BANDS_IN_ORDER, 2)) #PANSTARRS
+            create_panstarrs_csv(the_gals,the_band_pairs,get_csv_out_path())
+        else:
+            the_band_pairs = list(itertools.combinations(SDSS_BANDS_IN_ORDER, 2)) #SDSS
+            create_sdss_csv(the_gals,the_band_pairs,get_csv_out_path())
