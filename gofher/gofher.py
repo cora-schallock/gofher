b9e45bbdb506c18a10f5b3b5d92d0f259b3d56be
Cora Schallock <coraschallock@gmail.com>
2024-05-30 00:08:15 -0700

    added different gofher run modes: inital, fixed-center, and fixed - as well as improved documentation in gofher.py

--- gofher/gofher.py
+++ gofher/gofher.py
@@ -12,11 +12,17 @@ from sersic import get_gopher_params_from_sersic_fit
 
 
 
-def calculate_dist(cm,center):
-    """L2 Distance Norm"""
-    return np.linalg.norm(np.array(cm)-np.array(center))
+def calculate_dist(coord0: tuple, coord1: tuple)-> float:
+    """L2 Distance Norm between coord0 and coord1 coordinates
+        Args:
+            coord0: the first coordinate
+            coord1: the second coordinate
+        Returns:
+            the euclidian distance between cm and center
+    """
+    return np.linalg.norm(np.array(coord0)-np.array(coord1))
 
-def find_mask_spot_closest_to_center(the_mask,approx_center):
+def find_mask_spot_closest_to_center(the_mask: np.ndarray, approx_center: tuple) -> np.ndarray:
     """Uses connected regions to locate center most cluster
 
         Args:
@@ -35,13 +41,11 @@ def find_mask_spot_closest_to_center(the_mask,approx_center):
 
     return blobs_labels==unique[np.argmin(dist_to_center)]
 
-def run_default_gofher_parameters_fitting(the_gal):
+def run_default_gofher_parameters_fitting(the_gal: galaxy) -> galaxy:
     """Finds gofher_parameters for the galaxy that will be used to generate the ellipse masks and bisection masks
 
         Args:
-            shape: The shape of the ellipse mask
-            r: how much to scale semi-major/semi-minor axis length of ellipse
-                semi-major=r*a semi-minor=r*b
+            the_gal: the galaxy
         Returns:
             the galaxy with gofher_parameters found using default fitting process
     """
@@ -92,8 +96,84 @@ def run_default_gofher_parameters_fitting(the_gal):
 
     return the_gal
 
+def run_fixed_center_gofher_parameter_fitting(the_gal: galaxy, inital_gofher_params: gofher_parameters) -> galaxy:
+    """Using a fixed center from inital_gofher_params (x,y), fits a/b/theta from sep
+
+        Args:
+            the_gal: the galaxy
+            inital_gofher_params: the gofher_parameters provided that specify the fixed (x,y) center
+            IMPORTANT: This (x,y) center from inital_gofher_params is used in the heuristic that is used to select the primary sep object
+        Returns:
+            the galaxy with gofher_parameters found using fixed center
+    """
+
+    #Step 1: Setup inital necessary variables:
+    fixed_center_gofher_parameters = gofher_parameters()
+    data = copy.deepcopy(the_gal[the_gal.ref_band].data)
+
+    #Step 2: Run Sep on ref_band and find inital_gofher_parameters
+    (cm_x, cm_y) = (inital_gofher_params.x, inital_gofher_params.y)
+    the_el_sep, mu_bkg = run_sep(data, cm_x, cm_y)
+    fixed_center_gofher_parameters.load_from_sep_object(the_el_sep)
+
+    #Step 3: Manually Fix center to (x,y) from inital_gofher_params:
+    fixed_center_gofher_parameters.x = inital_gofher_params.x
+    fixed_center_gofher_parameters.y = inital_gofher_params.y
+
+    #Step 4: Set galaxy's gofher_parameters to those found fixed_center_gofher_parameters
+    the_gal.gofher_params = fixed_center_gofher_parameters
+
+    return the_gal
+
+def run_inital_gofher_parameters_fitting(the_gal: galaxy, inital_gofher_parameters: gofher_parameters) -> galaxy:
+    """Using a inital ellipse specified by inital_gofher_parameters, fits ellipse by comparing pdf>pdf_out and doing a bounded sersic fit
+
+        Args:
+            the_gal: the galaxy
+            inital_gofher_params: the gofher_parameters provided that specify the fixed (x,y) center
+            IMPORTANT: This (x,y) center from inital_gofher_params is used in the heuristic that is used to select the center most spot in pdf_in > pdf_out
+        Returns:
+            the galaxy with gofher_parameters found using inital ellipse from inital_gofher_parameters
+    """
+
+    #Step 1: Create an ellipse mask using the inital_gofher_parameters
+    data = copy.deepcopy(the_gal[the_gal.ref_band].data)
+    shape = the_gal.get_shape()
+    el_mask = inital_gofher_parameters.create_ellipse_mask(shape,r=1.0)
+    (cm_x, cm_y) = (inital_gofher_parameters.x,inital_gofher_parameters.y)
+
+    #Step 2: Using the inital_gofher_parameters fit two pdfs pdf_in (probability inside ellipse) and pdf_out (probability outside ellipse)
+    inside_ellipse = data[np.logical_and(el_mask,the_gal[the_gal.ref_band].valid_pixel_mask)].flatten()
+    loc, scale = expon.fit(inside_ellipse) #https://stackoverflow.com/questions/25085200/scipy-stats-expon-fit-with-no-location-parameter
+    pdf_in = expon.pdf(data, loc=loc, scale=scale)
+
+    outside_ellipse = data[np.logical_and(np.logical_not(el_mask),the_gal[the_gal.ref_band].valid_pixel_mask)].flatten()
+    loc, scale = expon.fit(outside_ellipse) #https://stackoverflow.com/questions/25085200/scipy-stats-expon-fit-with-no-location-parameter
+    pdf_out = expon.pdf(data, loc=loc, scale=scale)
+
+    #Step 3: Create a mask that only includes pixels in which there is a higher probability that it in inside the inital ellipse compared to the probability it is outside the ellipse
+    the_mask = pdf_out < pdf_in
+
+    center_mask = find_mask_spot_closest_to_center(the_mask,(cm_x, cm_y))
+    bright_spot_mask = np.logical_and(the_mask,np.logical_not(center_mask))
+
+    the_mask = np.logical_and(center_mask,the_gal[the_gal.ref_band].valid_pixel_mask)
+
+    #Step 4: Try fitting sersic:
+    try:
+        the_sersic_mask = np.logical_and(the_mask,the_gal[the_gal.ref_band].valid_pixel_mask)
+        the_gal.gofher_params = get_gopher_params_from_sersic_fit(inital_gofher_parameters,data,the_sersic_mask)
+        the_gal.gofher_params.a = inital_gofher_parameters.a
+        the_gal.gofher_params.b = inital_gofher_parameters.b
+    except:
+        print("Error fitting sersic, using provided inital_gofher_parameters")
+        the_gal.gofher_params = inital_gofher_parameters
+        the_gal.encountered_sersic_fit_error = True
+
+    return the_gal
+
 def run_gofher(name,fits_path_function,blue_to_red_bands_in_order,ref_bands_in_order,paper_label=None):
-    """Runs gofher of a single galaxy
+    """Runs gofher of a single galaxy - using default_gofher_parameters_fitting
 
         Args:
             name: the name of the galaxy
@@ -106,11 +186,14 @@ def run_gofher(name,fits_path_function,blue_to_red_bands_in_order,ref_bands_in_o
         Returns:
             the galaxy
     """
+    #Create galaxy:
     the_gal = galaxy(name,paper_label)
 
+    #Constuct individual galaxy bands:
     for band in blue_to_red_bands_in_order:
         the_gal.construct_band(band,fits_path_function(name,band))
 
+    #Find reference band for galaxy:
     for ref_band in ref_bands_in_order:
         if the_gal.has_valid_band(ref_band):
             the_gal.ref_band = ref_band
@@ -119,12 +202,69 @@ def run_gofher(name,fits_path_function,blue_to_red_bands_in_order,ref_bands_in_o
     if the_gal.ref_band == "":
         raise ValueError("run_gofher")
     
+    #Generate the_band_pairs:
     the_band_pairs = list(itertools.combinations(blue_to_red_bands_in_order, 2))
 
     #Use default gofher_parameter fitting:
     the_gal = run_default_gofher_parameters_fitting(the_gal)
 
-    #run gofher on the galaxy
+    #Run gofher on the galaxy:
+    the_gal.run_gofher(the_band_pairs)
+
+    return the_gal
+
+def run_gofher_with_parameters(name,fits_path_function,blue_to_red_bands_in_order,ref_band,inital_gofher_params,paper_label="",mode="fixed"):
+    """Runs gofher of a single galaxy - using fixed parameteres (such as SpArcFiRe derived parameters)
+
+        Args:
+            name: the name of the galaxy
+            fits_path_function: a function that takes in galaxy_name and waveband and returns the file paths of the fits files
+            blue_to_red_bands_in_order: a list of the name of wavebands in order of Bluest to Reddest wavebands
+            ref_band: the specific ref band to us2
+            inital_gofher_params: the inital gofher parameters
+                Note: This can be derived from a different fitting process such as SpArcFiRe
+            paper_label: the baseline near side label gofher is comparing its answer to
+                Important: If no baseline label, leave as none
+            mode: specifies the run mode of gofher fitting
+                inital: inital_gofher_params used as inital ellipse and further fitting is done - see: run_inital_gofher_parameters_fitting()
+                fixed-center: inital_gofher_params center is fixed, but uses a and b found from sep - see: run_fixed_center_gofher_parameter_fitting()
+                fixed: inital_gofher_params used as is by gofher
+        Returns:
+            the galaxy
+    """
+
+    #Check provided mode is valid:
+    modes = ["inital","fixed-center","fixed"]
+    
+    if mode.strip().lower() in modes:
+        mode = mode.strip().lower()
+    else:
+        print('Warning: run_gofher_with_parameters() mode {} is invalid, must be either ["inital","fixed-center","fixed"], using mode "fixed"')
+
+    #Create galaxy:
+    the_gal = galaxy(name,paper_label)
+    
+    #Constuct individual galaxy bands:
+    for band in blue_to_red_bands_in_order:
+        the_gal.construct_band(band,fits_path_function(name,band))
+
+    #Use provided ref_band:
+    the_gal.ref_band = ref_band
+    
+    #Generate the_band_pairs:
+    the_band_pairs = list(itertools.combinations(blue_to_red_bands_in_order, 2))
+
+    if mode == "inital":
+        #Use inital_gofher_params as inital guess for gofher fitting:
+        the_gal = run_inital_gofher_parameters_fitting(the_gal, inital_gofher_params)
+    elif mode == "fixed-center":
+        #Use fixed center gofher_parameter fitting:
+        the_gal = run_fixed_center_gofher_parameter_fitting(the_gal, inital_gofher_params)
+    else:
+        #Use provided inital_gofher_params:
+        the_gal.gofher_params = inital_gofher_params
+
+    #Run gofher on the galaxy:
     the_gal.run_gofher(the_band_pairs)
 
     return the_gal
