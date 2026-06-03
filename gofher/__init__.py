339d3f731124bfbc587c832f23f6fa15cabfc22b
Cora Schallock <coraschallock@gmail.com>
2026-06-03 11:22:47 -0700

    starting requirments.txt

---
+++ gofher/__init__.py
@@ -0,0 +1,12 @@
+"""
+GOFHER - Galactic Orientation from Histogram Estimated Reddening
+=====
+
+Provides
+  1. Determines the side of minor axis that has a higher concentration of red light
+
+Directive
+    When given multiple fits images of the same Spiral Galaxy, GOFHER determines
+    which side of the minor axis has a higher concentration of red light
+
+"""
---
+++ gofher/array.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def create_dist_array(h,k,shape):
+    """creates an array where each element is euclidian distance from point (h,k)
+    
+    Args:
+        h: x-axis value
+        k: y-axis value
+        
+    Returns:
+        distance matrix
+    """
+    #return np.sqrt(xv**2 + yv**2)
+    return
